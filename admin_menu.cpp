d928fe8083e33f8ec597f3219dfab33cf6fba937
OnlyT-T <trungonlyt145@gmail.com>
2025-04-27 22:49:51 +0700

    Small modification in file admin_menu.cpp - Attempt #2

--- admin_menu.cpp
+++ admin_menu.cpp
@@ -5,19 +5,21 @@
 #include <string>
 
 void inputChoice(const int &);
-void adminLogin();
-void customerLogin();
-void exitProgram();
+void checkInventory();
+void searchProduct();
+void addProduct();
+void removeProduct();
+void logOutAdmin();
 
 int main() {
     while(true) {
-        cout << " -----------------------------------------------------------\n";
-        cout << " - Welcome to the 'Laptop & Accessories Retailer' Program. -\n";
-        cout << " ------------------------< Menu >---------------------------\n";
-        cout << " 1. Login as Admin.\n";
-        cout << " 2. Login as Customer.\n";
-        cout << " 3. Exit Program.\n";
-        cout << " * Enter your choice: ";
+        cout << "------------------------< Admin >--------------------------\n";
+        cout << " 1. Check Inventory\n";
+        cout << " 2. Search Product (using product's ID)\n";
+        cout << " 3. Add a new product\n";
+        cout << " 4. Remove an existing product\n";
+        cout << " 5. Log out\n";
+        cout << " *  Enter your choice: ";
 
         // Handling invalid input
         string input; // Define the variable named 'input' as string
@@ -25,9 +27,10 @@ int main() {
 
         // Validate input to ensure it's a single character
         if (input.length() != 1) {
-            cout << "Invalid choice. Please enter a number from 1 to 3.\n";
+            cout << "Invalid choice. Please enter a number from 1 to 5.\n\n";
             continue;
         }
+        cout << endl;
         int choice = input[0] - '0';
         inputChoice(choice);
     }
@@ -38,29 +41,47 @@ int main() {
 void inputChoice(const int &choice) {
     switch (choice) {
         case 1: 
-            adminLogin();
+            checkInventory();
             break;
         case 2:
-            customerLogin();
+            searchProduct();
             break;
         case 3:
-            exitProgram();
+            addProduct();
+            break;
+        case 4:
+            removeProduct();
+            break;
+        case 5:
+            logOutAdmin();
             break;
         default:
-            cout << "Invalid choice. Please enter a number from 1 to 3.\n";
+            cout << "Invalid choice. Please enter a number from 1 to 5.\n";
     }
 }
 
-void adminLogin() {
-    cout << "-------------------------------------------------------\n";
-    cout << "Admin login functionality goes here.\n";
+void checkInventory() {
+    cout << "-----------------------------------------------------------\n";
+    cout << "Checking the Inventory...\n";
+    cout << endl;
 }
-void customerLogin() {
-    cout << "-------------------------------------------------------\n";
-    cout << "Customer login functionality goes here.\n";
+void searchProduct() {
+    cout << "-----------------------------------------------------------\n";
+    cout << "Searching for a product in the Inventory...\n";
+    cout << endl;
 }
-void exitProgram() {
-    cout << "-------------------------------------------------------\n";
-    cout << "Exiting the program. Goodbye!\n";
+void addProduct() {
+    cout << "-----------------------------------------------------------\n";
+    cout << "Adding a new product into the Inventory...\n";
+    cout << endl;
+}
+void removeProduct() {
+    cout << "-----------------------------------------------------------\n";
+    cout << "Removing an existing product from the Inventory...\n";
+    cout << endl;
+}
+void logOutAdmin() {
+    cout << "-----------------------------------------------------------\n";
+    cout << "Logged Out Successfully!\n";
     exit(0);
 }
